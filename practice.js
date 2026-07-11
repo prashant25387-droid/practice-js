15d4c891e3e33b12f139f2a059a5f381fdfb3193
prashant25387-droid <prashant25387@iiitd.ac.in>
2026-07-11 14:31:10 +0530

    Implement API call to post film data
    Added API call to fetch and post data about a film.

--- practice.js
+++ practice.js
@@ -8,3 +8,30 @@ const favouritefilm={
 const{title,year,star,director} = favouritefilm
 
 console.log(`My favourite film is ${title} starring ${star}. it is directed by ${director}`)
+
+
+
+try{
+    const response = await fetch("https://apis.scrimba.com/jsonplaceholder/posts",{
+        method: 'POST',
+        body: JSON.stringify({
+            title: "Holiday Nightmares",
+            body: "When I was Kidnapped in Scotland",
+            userid: 890
+        }),
+        headers:{
+            "Content-Type": "application/json"
+        }
+        
+    })
+    if(!response.ok){
+        throw new Error("There is an API issue")
+    }
+    const data = await response.json()
+    console.log(data)
+} catch(err){
+    console.log(err)
+
+} finally{
+    console.log("the operation completed")
+}
